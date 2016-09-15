c6189d8d916d35fe3d05a2bc1e2e9fb27a07d77b
Ewan Valentine <ewan@theladbible.com>
2016-09-15 17:49:37 +0100

    init

--- src/game.js
+++ src/game.js
@@ -25,13 +25,24 @@ window.addEventListener('DOMContentLoaded', () => {
       const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
       // create a built-in "sphere" shape; its constructor takes 5 params: name, width, depth, subdivisions, scene
-      const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+      const box = BABYLON.Mesh.CreateBox('tank', 1.0, scene);
 
       // move the sphere upward 1/2 of its height
-      sphere.position.y = 1;
+      box.position.y = 1;
+      box.position.x = 0;
+
+      // Grass material
+      const materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
+      materialPlane.diffuseTexture = new BABYLON.Texture("./src/textures/grass.jpg", scene);
+      materialPlane.diffuseTexture.uScale = 5.0;//Repeat 5 times on the Vertical Axes
+      materialPlane.diffuseTexture.vScale = 5.0;//Repeat 5 times on the Horizontal Axes
+      materialPlane.backFaceCulling = false;//Always show the front and the back of an element
 
       // create a built-in "ground" shape; its constructor takes the same 5 params as the sphere's one
-      const ground = BABYLON.Mesh.CreateGround('ground1', 100, 100, 2, scene);
+      const plane = BABYLON.Mesh.CreatePlane('ground1', 120, scene);
+      plane.position.y = 0;
+      plane.rotation.x = Math.PI / 2;
+      plane.material = materialPlane;
 
       // return the created scene
       return scene;
@@ -45,4 +56,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // the canvas/window resize event handler
   window.addEventListener('resize', () => engine.resize());
+
+  const unit = (name) => scene.getMeshByName(name);
+
+  const forwards = (unit, amount) => unit.position.x = amount;
+
+  setTimeout(() => forwards(unit('tank'), 10), 5000);
 });
+

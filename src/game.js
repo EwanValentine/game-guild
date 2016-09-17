7d255159adea854f839de7887346ac474f244479
Ewan Valentine <ewan.valentine89@gmail.com>
2016-09-17 10:18:31 +0100

    Re-organising

--- src/game.js
+++ src/game.js
@@ -1,10 +1,15 @@
-window.addEventListener('DOMContentLoaded', () => {
+// Globals
+const POINTER = "POINTER";
+const SELECT_UNITS = "SELECT_UNITS";
 
-  const state = {
-    selected: [],
-    units: [],
-    factories: [],
-  }
+const state = {
+  selected: [],
+  selectMode: SELECT_UNITS,
+  units: [],
+  factories: [],
+}
+
+window.addEventListener('DOMContentLoaded', () => {
   
   // get the canvas DOM element
   const canvas = document.getElementById('renderCanvas');
@@ -15,37 +20,37 @@ window.addEventListener('DOMContentLoaded', () => {
   // createScene function that creates and return the scene
   const createScene = () => {
 
+      const speedCharacter = 8;
+      const gravity = 0.15;
+      const meshesColliderList = [];
+
       // create a basic BJS Scene object
       const scene = new BABYLON.Scene(engine);
 
+      scene.collisionsEnabled = true;
+
       // Enable physics engine
-      scene.enablePhysics(new BABYLON.Vector3(0,-10,0), new BABYLON.OimoJSPlugin());
+      scene.enablePhysics(new BABYLON.Vector3(0, -10, 0), new BABYLON.OimoJSPlugin());
+
+      for (var i = 1; i < scene.meshes.length; i++) {
+        if (scene.meshes[i].checkCollisions && scene.meshes[i].isVisible === false) {
+          scene.meshes[i].setPhysicsState(BABYLON.PhysicsEngine.BoxImpostor, { mass: 0, 
+                                          friction: 0.5, restitution: 0.7 });
+          meshesColliderList.push(scene.meshes[i]);
+        }
+      }
 
       // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
       const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 10, -10), scene);
 
       // target the camera to scene origin
       camera.setTarget(BABYLON.Vector3.Zero());
-
-      // attach the camera to the canvas
       camera.attachControl(canvas, false);
+      camera.applyGravity = true;
+      camera.checkCollisions = true;
 
       // create a basic light, aiming 0,1,0 - meaning, to the sky
       const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-
-      // create a built-in "sphere" shape; its constructor takes 5 params: name, width, depth, subdivisions, scene
-      const box = BABYLON.Mesh.CreateBox('tank', 0.5, scene);
-      const boxMat = new BABYLON.StandardMaterial("ground", scene);
-      boxMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-      box.material = boxMat;
-      box.type = "unit";
-
-      // Check collision
-      box.checkCollisions = true;
-
-      // move the sphere upward 1/2 of its height
-      box.position.y = 0.5;
-      box.position.x = 0;
 
       // Grass material
       const materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
@@ -58,6 +63,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const plane = BABYLON.Mesh.CreateGround('ground1', 120, 120, 2, scene);
       plane.material = materialPlane;
       plane.setPhysicsState({ impostor: BABYLON.PhysicsEngine.BoxImpostor, move: false });
+      plane.checkCollisions = true;
 
       // return the created scene
       return scene;
@@ -65,49 +71,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // call the createScene function
   const scene = createScene();
-
-  const unit = (name) => scene.getMeshByName(name);
-
-  const moveX = (unit, amount) => unit.position.x = amount;
-  const moveY = (unit, amount) => unit.position.y = amount;
-  const moveZ = (unit, amount) => unit.position.z = amount;
-
-  const selectUnit = (unit) => {
-    if (unit.type === "unit") {
-      state.selected.unshift(unit);
-    }
-  }
-
-  scene.onPointerDown = (evt, pickResult) => {
-
-    const impact = pickResult.pickedMesh;
-    const position = pickResult.pickedPoint;
-
-    selectUnit(impact);
-
-    if (pickResult.hit) {
-
-      // For each selected unit
-      state.selected.map(item => {
-
-        // Animate to picked location
-        BABYLON.Animation.CreateAndStartAnimation(
-          "anim", 
-          item, 
-          "position", 
-          30, 
-          30, 
-
-          // Old position
-          item.position, 
-          
-          // New position
-          new BABYLON.Vector3(position.x, 1, position.z), 
-          0
-        );   
-      });     
-    }     
-  }
 
   // run the render loop
   engine.runRenderLoop(() => scene.render());
--- src/units.js
+++ src/units.js
@@ -1,1 +1,60 @@
-const createUnit = () => {}
+const unit = (name) => scene.getMeshByName(name);
+
+// Select a unit
+const selectUnit = (unit) => {
+  if (unit.type === "unit") {
+    state.selected.unshift(unit);
+  }
+}
+
+// Create unit
+const createUnit = (scene) => {
+  const box = BABYLON.Mesh.CreateBox('tank', 0.5, scene);
+  const boxMat = new BABYLON.StandardMaterial("ground", scene);
+  boxMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
+  box.material = boxMat;
+  box.type = "unit";
+
+  // Check collision
+  box.checkCollisions = true;
+
+  // move the sphere upward 1/2 of its height
+  box.position.y = 0.5;
+  box.position.x = 0;
+
+  box.setPhysicsState(BABYLON.PhysicsEngine.BoxImpostor);
+}
+
+createUnit(scene);
+createUnit(scene);
+
+scene.onPointerDown = (evt, pickResult) => {
+
+  const impact = pickResult.pickedMesh;
+  const position = pickResult.pickedPoint;
+
+  selectUnit(impact);
+
+  if (pickResult.hit) {
+
+    // For each selected unit
+    state.selected.map(item => {
+
+      // Animate to picked location
+      BABYLON.Animation.CreateAndStartAnimation(
+        "anim", 
+        item, 
+        "position", 
+        30, 
+        30, 
+
+        // Old position
+        item.position, 
+        
+        // New position
+        new BABYLON.Vector3(position.x, 1, position.z), 
+        0
+      );   
+    });     
+  }     
+}
